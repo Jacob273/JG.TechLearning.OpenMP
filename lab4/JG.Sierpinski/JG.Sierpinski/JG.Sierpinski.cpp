7b1bc838ba02ed9e52c2f53594a5cd5be8027cd9
jacob273 <jakubgmur@op.pl>
2021-01-14 19:10:14 +0100

    added JG.Sierpinski solution. implementation draws only a single triangle at the moment

---
+++ lab4/JG.Sierpinski/JG.Sierpinski/JG.Sierpinski.cpp
@@ -0,0 +1,52 @@
+#include <GL/glut.h>
+
+// Clears the current window and draws a triangle.
+void display() {
+
+    // Set every pixel in the frame buffer to the current clear color.
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Drawing is done by specifying a sequence of vertices
+    glBegin(GL_TRIANGLES);
+        
+        glColor3f(3, 5, 155);
+        
+        const int numberOfVectors = 3;
+        const int dimensions = 2;
+        const GLfloat triangleVectors[numberOfVectors][dimensions] = { {-0.5, -0.7}, {0.5, -0.7}, {0.0, 0.5} };
+        glVertex2fv(triangleVectors[0]);
+        glVertex2fv(triangleVectors[1]);
+        glVertex2fv(triangleVectors[2]);
+    glEnd();
+
+    // Flush drawing command buffer to make drawing happen as soon as possible.
+    glFlush();
+}
+
+// Initializes GLUT, the display mode, and main window; registers callbacks;
+// enters the main event loop.
+int main(int argc, char** argv) {
+
+    // Use a single buffered window in RGB mode
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    const int windowPositionX = 500;
+    const int windowPositionY = 200;
+    glutInitWindowPosition(windowPositionX, windowPositionY);
+
+    const int windowWidth = 400;
+    const int windowHeight = 500;
+
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutCreateWindow("A Simple Triangle");
+
+    // Tell GLUT that whenever the main window needs to be repainted that it
+    // should call the function display().
+    glutDisplayFunc(display);
+
+    // Tell GLUT to start reading and processing events.  This function
+    // never returns; the program only exits when the user closes the main
+    // window or kills the process.
+    glutMainLoop();
+}
