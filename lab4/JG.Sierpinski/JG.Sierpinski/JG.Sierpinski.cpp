e33d0c1edac836fb4505af58d2bbde60c33b1bc6
jacob273 <jakubgmur@op.pl>
2021-01-15 20:50:46 +0100

    updated sierpinski code: 1. added VerticesGenerator to generate easily vertices 2. added Triangle class which may be initialzied with three vertices and does expose A,B and C vertices. It also expose Mid points for AB, AC and CB.

--- lab4/JG.Sierpinski/JG.Sierpinski/JG.Sierpinski.cpp
+++ lab4/JG.Sierpinski/JG.Sierpinski/JG.Sierpinski.cpp
@@ -1,7 +1,119 @@
 #include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+
+/**
+    X = (A + B )/ 2
+    Y = (A + C) / 2
+**/
+GLfloat GetMidpoint(GLfloat coord1, GLfloat coord2)
+{
+    return (coord1 + coord2) / 2;
+}
+
+class VerticesGenerator {
+
+public: 
+    GLfloat* Generate(GLfloat a, GLfloat b)
+    {
+        GLfloat* tmp = new GLfloat[2];
+        tmp[0] = a;
+        tmp[1] = b;
+        return tmp;
+    }
+};
+
+class Triangle {
+
+private: 
+
+    GLfloat GetMidPointAB_x()
+    {
+        return (A[0] + B[0]) / 2;
+    }
+
+    GLfloat GetMidPointAB_y()
+    {
+        return (A[1] + B[1]) / 2;
+    }
+
+    GLfloat GetMidPointAC_x()
+    {
+        return (A[0] + C[0]) / 2;
+    }
+
+    GLfloat GetMidPointAC_y()
+    {
+        return (A[1] + C[1]) / 2;
+    }
+
+    GLfloat GetMidPointBC_x()
+    {
+        return (B[0] + C[0]) / 2;
+    }
+
+
+    GLfloat GetMidPointBC_y()
+    {
+        return (B[1] + C[1]) / 2;
+    }
+
+public:
+
+    GLfloat* A;
+    GLfloat* B;
+    GLfloat* C;
+
+    Triangle(GLfloat* vertexA, GLfloat* vertexB, GLfloat* vertexC)
+    {
+        A = vertexA;
+        B = vertexB;
+        C = vertexC;
+    }
+
+    GLfloat* GetMidPointsAB() 
+    {
+        GLfloat* tmp = new GLfloat[2];
+        tmp[0] = GetMidPointAB_x();
+        tmp[1] = GetMidPointAB_y();
+        return tmp;
+    }
+
+    GLfloat* GetMidPointsAC()
+    {
+        GLfloat* tmp = new GLfloat[2];
+        tmp[0] = GetMidPointAC_x();
+        tmp[1] = GetMidPointAC_y();
+        return tmp;
+    }
+
+    GLfloat* GetMidPointsBC()
+    {
+        GLfloat* tmp = new GLfloat[2];
+        tmp[0] = GetMidPointBC_x();
+        tmp[1] = GetMidPointBC_y();
+        return tmp;
+    }
+
+    void PrintMyVertices() {
+        std::cout << "Ax = " << A[0] << " Ay= " << A[1] << std::endl;
+        std::cout << "Bx = " << B[0] << " By= " << B[1] << std::endl;
+        std::cout << "Cx = " << C[0] << " Cy= " << C[1] << std::endl;
+        std::cout << std::endl;
+    }
+
+    void PrintMidpoints() {
+        std::cout << "ABx = " << GetMidPointsAB()[0] << " ABy= " << GetMidPointsAB()[1] << std::endl;
+        std::cout << "ACx = " << GetMidPointsAC()[0] << " ACy= " << GetMidPointsAC()[1] << std::endl;
+        std::cout << "BCx = " << GetMidPointsBC()[0] << " BCy= " << GetMidPointsAB()[1] << std::endl;
+        std::cout << std::endl;
+    }
+};
 
 // Clears the current window and draws a triangle.
-void DrawSingleTriangle() {
+void DrawTwoTriangles() {
+
+    VerticesGenerator generator = VerticesGenerator();
 
     // Set every pixel in the frame buffer to the current clear color.
     glClear(GL_COLOR_BUFFER_BIT);
@@ -15,25 +127,35 @@ void DrawSingleTriangle() {
         const int dimensions = 2;
 
         //dlugosc boku AB
-        const float sideLength = 0.6;
+        GLfloat sideLength = 0.6;
 
         //poczatek ukladu wspolrzednych
-        const GLfloat begginingOfCoordinateSystem[2] = { -0.5, 0 };
+        GLfloat begginingOfCoordinateSystem[2] = { -0.5, 0 };
+        
+        GLfloat* A = generator.Generate(begginingOfCoordinateSystem[0] + (GLfloat)(-0.2), begginingOfCoordinateSystem[1]);
+        GLfloat* B = generator.Generate(begginingOfCoordinateSystem[0] + (GLfloat)(-0.2), begginingOfCoordinateSystem[1] + sideLength);
+        GLfloat* C = generator.Generate(begginingOfCoordinateSystem[0] + (GLfloat)0.7, begginingOfCoordinateSystem[1]);
 
-        const GLfloat triangleVertices[numberOfVectors][dimensions] = { {begginingOfCoordinateSystem[0] + (-0.2), begginingOfCoordinateSystem[1]}, {begginingOfCoordinateSystem[0]  +(-0.2), begginingOfCoordinateSystem[1] + sideLength}, {begginingOfCoordinateSystem[0] + 0.7, begginingOfCoordinateSystem[1]} };
-        glVertex2fv(triangleVertices[0]);
-        glVertex2fv(triangleVertices[1]);
-        glVertex2fv(triangleVertices[2]);
+        Triangle* mainTriangle = new Triangle(A, B ,C);
+        mainTriangle->PrintMyVertices();
+        mainTriangle->PrintMidpoints();
 
-        const float sideLength2 = 0.3;
+        // Rysowanie trojkata glownego
+        glVertex2fv(mainTriangle->A);
+        glVertex2fv(mainTriangle->B);
+        glVertex2fv(mainTriangle->C);
+
+        GLfloat* Aprim = mainTriangle->GetMidPointsAB();
+        GLfloat* Bprim = mainTriangle->GetMidPointsBC();
+        GLfloat* Cprim = mainTriangle->GetMidPointsAC();
+
+
+        Triangle* subTriangle = new Triangle(Aprim, Bprim, Cprim);
+        
         glColor3f(1.0, 0, 0);
-        const GLfloat triangleVertices2[numberOfVectors][dimensions] = { {begginingOfCoordinateSystem[0] + (-0.2), begginingOfCoordinateSystem[1]}, {begginingOfCoordinateSystem[0] + (-0.2), begginingOfCoordinateSystem[1] + sideLength2}, {begginingOfCoordinateSystem[0] + 0.7, begginingOfCoordinateSystem[1]} };
-        glVertex2fv(triangleVertices2[0]);
-        glVertex2fv(triangleVertices2[1]);
-        glVertex2fv(triangleVertices2[2]);
-        //draw smaller one
-
-
+        glVertex2fv(subTriangle->A);
+        glVertex2fv(subTriangle->B);
+        glVertex2fv(subTriangle->C);
 
     glEnd();
 
@@ -63,7 +185,7 @@ int main(int argc, char** argv) {
 
     // Tell GLUT that whenever the main window needs to be repainted that it
     // should call the function display().
-    glutDisplayFunc(DrawSingleTriangle);
+    glutDisplayFunc(DrawTwoTriangles);
 
     // Tell GLUT to start reading and processing events.  This function
     // never returns; the program only exits when the user closes the main
